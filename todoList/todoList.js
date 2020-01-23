5216d80f13b959ebf02e40708ffca115fe0e81c6
Rodolfo Marinho <rodolfo@grupomarinho.com.br>
2020-01-23 08:14:08 -0300

    Save tasks to local storage.

--- todoList/todoList.js
+++ todoList/todoList.js
@@ -1,29 +1,37 @@
-var tasks = [];
+var tasks = JSON.parse(localStorage.getItem("todo_list")) || [];
+var taskListElement = document.querySelector("div#taskListDiv ul");
 document.querySelector("div#newTaskDiv button#addTaskButton").onclick = addTask;
 listTasks();
 
 function listTasks() {
-    var taskListElement = document.querySelector("div#taskListDiv ul");
     taskListElement.innerHTML = "";
-    tasks.forEach(element => {
+    for (task of tasks) {
         var taskElement = document.createElement("li");
-        taskElement.appendChild(document.createTextNode(element));
+        taskElement.appendChild(document.createTextNode(task));
         taskListElement.appendChild(taskElement);
         var deleteTaskButtonElement = document.createElement("button");
         deleteTaskButtonElement.appendChild(document.createTextNode("Delete item"));
-        deleteTaskButtonElement.appendChild(document.createTextNode("Delete item"));
-        deleteTaskButtonElement.onclick = function () {
-            tasks.splice(tasks.indexOf(element + " "), 1);
-            listTasks();
-        };
+        task_idx = tasks.indexOf(task);
+        deleteTaskButtonElement.setAttribute("onclick", "deleteTask(" + task_idx + ")");
         taskElement.appendChild(deleteTaskButtonElement);
-    });
+    }
 }
 
 function addTask() {
     var inputElement = document.querySelector("div#newTaskDiv input");
     tasks.push(inputElement.value + " ");
     inputElement.value = "";
-    console.log(tasks);
     listTasks();
+    saveToStorage();
+}
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    listTasks();
+    saveToStorage();
+}
+
+function saveToStorage(){
+
+    localStorage.setItem("todo_list", JSON.stringify(tasks));
 }
